0f4f9d41d1beb1516e798733e45b4afe834e3264
Zoonmy <pjm2571@naver.com>
2024-03-04 19:10:10 +0900

    feat: Pawn Test 클래스 create 메소드의 중복제거를 위한 verifyPawn 메소드 구현 - pawnColor 값이 중복되는 결과 값 확인 부분을 제거하기 위한 메소드 구현

--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -1,24 +1,21 @@
 import org.junit.jupiter.api.*;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class PawnTest {
     private final static String BLACK = "black";
     private final static String WHITE = "white";
 
     @Test
-    @DisplayName("흰색 폰과 검정색 폰의 생성")
+    @DisplayName("흰색 폰과 검정색 폰의 생성 테스트")
     public void create() {
-        StringBuilder pawnColor = new StringBuilder("white");
+        verifyPawn(WHITE);
+        verifyPawn(BLACK);
+    }
 
-        Pawn whitePawn = new Pawn(pawnColor.toString());
-        assertThat(whitePawn.getColor()).isEqualTo(WHITE);
-
-        pawnColor.setLength(0);
-
-        pawnColor.append("black");
-        Pawn blackPawn = new Pawn(pawnColor.toString());
-        assertThat(blackPawn.getColor()).isEqualTo(BLACK);
+    private void verifyPawn(String color) {
+        Pawn pawn = new Pawn(color);
+        assertThat(pawn.getColor()).isEqualTo(color);
     }
 
 }
