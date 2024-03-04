82c2a327e03f39719dc25bca7d4b199ccc8b4779
Zoonmy <pjm2571@naver.com>
2024-03-04 19:01:15 +0900

    refactor: Pawn Test 클래스의 create 메소드에 local variable 추가 - StringBuilder를 사용하여 pawnColor를 할당받는 local variable 추가 - String의 재할당 방지를 위해 StringBuilder로 구현

--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -3,13 +3,22 @@ import org.junit.jupiter.api.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PawnTest {
+    private final static String BLACK = "black";
+    private final static String WHITE = "white";
+
     @Test
     @DisplayName("흰색 폰과 검정색 폰의 생성")
     public void create() {
-        Pawn whitePawn = new Pawn("white");
-        assertThat(whitePawn.getColor()).isEqualTo("white");
+        StringBuilder pawnColor = new StringBuilder("white");
 
-        Pawn blackPawn = new Pawn("black");
-        assertThat(blackPawn.getColor()).isEqualTo("black");
+        Pawn whitePawn = new Pawn(pawnColor.toString());
+        assertThat(whitePawn.getColor()).isEqualTo(WHITE);
+
+        pawnColor.setLength(0);
+
+        pawnColor.append("black");
+        Pawn blackPawn = new Pawn(pawnColor.toString());
+        assertThat(blackPawn.getColor()).isEqualTo(BLACK);
     }
+
 }
