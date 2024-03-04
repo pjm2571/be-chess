0740cb9d772dfe2517924821da2b78bbd439f1ad
Zoonmy <pjm2571@naver.com>
2024-03-04 18:42:17 +0900

    refactor: Pawn Test 클래스의 create 메소드 수정 - White 값을 매개변수로 주어 Pawn의 getColor 메소드 테스트 - Black 값을 매개변수로 주어 Pawn의 getColor 메소드 테스트

--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -4,9 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class PawnTest {
     @Test
-    @DisplayName("흰색 폰이 생성되어야 한다")
+    @DisplayName("흰색 폰과 검정색 폰의 생성")
     public void create() {
-        Pawn pawn = new Pawn("white");
-        assertThat(pawn.getColor()).isEqualTo("white");
+        Pawn whitePawn = new Pawn("white");
+        assertThat(whitePawn.getColor()).isEqualTo("white");
+
+        Pawn blackPawn = new Pawn("black");
+        assertThat(blackPawn.getColor()).isEqualTo("black");
     }
 }
