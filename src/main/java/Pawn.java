0ed9705e5ce10b51e4255726bea36e7e04d50ffa
pjm2571 <pjm2571@naver.com>
2024-03-04 23:56:31 +0900

    feat: Pawn 객체 생성 시, 예외 처리를 해주는 메소드 구현 - 생성자로 입력된 pawnColor 값이 white/black이 아닐 경우에 예외 처리   되도록 구현 - 상수 선언을 Pawn 객체에 해주어 객체 생성 시 검증이 되도록 구현

--- src/main/java/Pawn.java
+++ src/main/java/Pawn.java
@@ -1,8 +1,19 @@
 public class Pawn {
+    private final static String WHITE_COLOR = "white";
+    private final static String BLACK_COLOR = "black";
     private String pawnColor;
 
     Pawn(String pawnColor) {
-        this.pawnColor = pawnColor;
+        setPawnColor(pawnColor);
+    }
+
+    private void setPawnColor(String pawnColor) {
+        String inputPawnColor = pawnColor.toLowerCase().replace(" ", "");
+
+        if (!inputPawnColor.equals(WHITE_COLOR) && !inputPawnColor.equals(BLACK_COLOR)) {
+            throw new IllegalArgumentException("[ERROR] 폰의 색은 white, black 색상만 가능합니다");
+        }
+        this.pawnColor = inputPawnColor;
     }
 
     public String getColor() {
