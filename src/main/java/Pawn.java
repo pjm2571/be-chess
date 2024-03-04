c0fe26310c132426f7c4fd26370abdde59a164e7
Zoonmy <pjm2571@naver.com>
2024-03-04 18:38:40 +0900

    feat: Pawn 클래스 구현 - 생성자로 Color 값을 가지는 Pawn 객체 구현

---
+++ src/main/java/Pawn.java
@@ -0,0 +1,11 @@
+public class Pawn {
+    private String pawnColor;
+
+    Pawn(String pawnColor) {
+        this.pawnColor = pawnColor;
+    }
+
+    public String getColor() {
+        return pawnColor;
+    }
+}
