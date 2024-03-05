d79c362873a8a478e7e89980e5142bb10c61a1ab
Zoonmy <pjm2571@naver.com>
2024-03-05 16:57:58 +0900

    feat: 자바 리플렉션을 활용한 Pawn의 color 값 테스트 구현 - 자바 리플렉션을 활용하여 Pawn의 color값을 getter 없이 테스트 하는 코드 구현

--- src/test/java/chess/pieces/PawnTest.java
+++ src/test/java/chess/pieces/PawnTest.java
@@ -3,27 +3,48 @@ package chess.pieces;
 import colors.color;
 import org.junit.jupiter.api.*;
 
-import static org.assertj.core.api.Assertions.*;
+import java.lang.reflect.Field;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PawnTest {
     @Test
     @DisplayName("흰색 폰과 검정색 폰의 생성 테스트")
-    public void create() {
+    public void create() throws Exception {
         verifyPawn(color.WHITE);
         verifyPawn(color.BLACK);
     }
 
-    private void verifyPawn(color color) {
+    private void verifyPawn(color color) throws Exception {
         Pawn pawn = new Pawn(color);
-        assertThat(pawn.getColor()).isEqualTo(color);
+
+        Object pawnColor = getPawnColor(pawn);
+
+        assertEquals(color, pawnColor);
+    }
+
+    private Object getPawnColor(Pawn pawn) throws Exception {
+        /* chess/pieces/Pawn.java 클래스를 찾는다 */
+        Class<?> pawnClass = Class.forName("chess.pieces.Pawn");
+
+        /* pawnClass의 pawnColor field를 찾아온다 */
+        Field field = pawnClass.getDeclaredField("pawnColor");
+
+        /* field의 접근 제한자가 private이기 때문에, setAccessible을 true로 설정한다. 하지 않으면 오류! */
+        field.setAccessible(true);
+
+        /* field의 color 값을 가져온다. color enum 타입이므로 Object로 받아온다. */
+        return field.get(pawn);
     }
 
     @Test
     @DisplayName("매개변수 없는 폰의 생성 테스트")
-    public void create_with_non_color() {
+    public void create_with_non_color() throws Exception {
         Pawn pawn = new Pawn();
-        assertEquals(color.WHITE, pawn.getColor());
+
+        Object pawnColor = getPawnColor(pawn);
+
+        assertEquals(color.WHITE, pawnColor);
     }
 
 
