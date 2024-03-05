fd9ca401ead0b19547f429ab693da768e086bbdd
Zoonmy <pjm2571@naver.com>
2024-03-05 15:45:16 +0900

    refactor: Pawn 클래스의 생성자 수정에 따른 테스트 클래스 수정 - Pawn 객체의 생성을 테스트하는 PawnTest 클래스 수정 	- 공백 및 UpperCase를 확인하는 테스트 삭제 - Pawn 객체 생성 후 Board 객체를 검증하는 BoardTest 클래스 수정

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,6 +1,7 @@
 package chess;
 
 import chess.pieces.Pawn;
+import colors.color;
 import org.junit.jupiter.api.*;
 
 
@@ -15,12 +16,12 @@ public class BoardTest {
     @Test
     @DisplayName("체스 보드 생성 후, 폰을 추가하는 테스트")
     public void create() {
-        Pawn white = new Pawn(Pawn.WHITE_COLOR);
+        Pawn white = new Pawn(color.WHITE);
         board.add(white);
         Assertions.assertEquals(1, board.size());
         Assertions.assertEquals(white, board.findPawn(0));
 
-        Pawn black = new Pawn(Pawn.BLACK_COLOR);
+        Pawn black = new Pawn(color.BLACK);
         board.add(black);
         Assertions.assertEquals(2, board.size());
         Assertions.assertEquals(black, board.findPawn(1));
