97e215ce1555d5fb80e450471c0a04f4fe7a6d3f
Zoonmy <pjm2571@naver.com>
2024-03-05 13:46:10 +0900

    feat: Board Test 코드에서 Before Each문 구현

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -5,20 +5,25 @@ import org.junit.jupiter.api.*;
 
 
 public class BoardTest {
+    private Board board;
+
+    @BeforeEach
+    public void initBoard() {
+        board = new Board();
+    }
+
     @Test
     @DisplayName("체스 보드 생성 후, 폰을 추가하는 테스트")
     public void create() {
-        Board board = new Board();
-
         Pawn white = new Pawn(Pawn.WHITE_COLOR);
         board.add(white);
-        Assertions.assertEquals(1, board.getSize());
+        Assertions.assertEquals(1, board.size());
         Assertions.assertEquals(white, board.findPawn(0));
 
         Pawn black = new Pawn(Pawn.BLACK_COLOR);
         board.add(black);
-        Assertions.assertEquals(2, board.getSize());
-
+        Assertions.assertEquals(2, board.size());
         Assertions.assertEquals(black, board.findPawn(1));
     }
+
 }
