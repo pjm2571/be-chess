314725084c1be09ebc035aaf3dcde27f6fb8d6bc
Zoonmy <pjm2571@naver.com>
2024-03-06 11:10:51 +0900

    feat: Board를 initialize하는 기능 구현 - initialize() 메소드를 통해 동작 - . 을 채워주는 setBlank() 메소드와 폰의 유니코드를 채워주는 setPawn() 메소드를 통해 initialize() 메소드가 동작 - String[8][8] 2차원 배열을 통해 chessBoard 판이 생성되는 것을 구현

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -1,15 +1,19 @@
 package chess;
 
 import chess.pieces.Pawn;
+import chess.pieces.Symbols;
+import colors.Colors;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Board {
-    private List<Pawn> pawns;
+    private List<Pawn> pawns = new ArrayList<>();
+    private String[][] chessBoard = new String[8][8];
 
     Board() {
-        pawns = new ArrayList<>();
+        initialize();
     }
 
     public void add(Pawn pawn) {
@@ -22,5 +26,34 @@ public class Board {
 
     public Pawn findPawn(int pawnIndex) {
         return pawns.get(pawnIndex);
+    }
+
+    private void initialize() {
+        setBlank(0);
+        setPawn(1, Colors.BLACK);
+        setBlank(2);
+        setBlank(3);
+        setBlank(4);
+        setBlank(5);
+        setPawn(6, Colors.WHITE);
+        setBlank(7);
+    }
+
+    private void setBlank(int rowIndex) {
+        for (int i = 0; i < 8; i++) {
+            chessBoard[rowIndex][i] = ".";
+        }
+    }
+
+    private void setPawn(int rowIndex, Colors pawnColor) {
+        for (int i = 0; i < 8; i++) {
+            Pawn pawn = new Pawn(pawnColor);
+
+            chessBoard[rowIndex][i] = pawn.getPawnSymbol();
+        }
+    }
+
+    public String[][] getChessBoard() {
+        return chessBoard;
     }
 }
