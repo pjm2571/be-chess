476b4e90720e44a3df56ae39436d4a33a99101f3
Zoonmy <pjm2571@naver.com>
2024-03-06 18:12:58 +0900

    feat: 체스의 말판들 enum에 추가 - pawn, knight, rook, bishop, queen, king 각각 white, black 종류 추가

--- src/main/java/chess/pieces/Symbols.java
+++ src/main/java/chess/pieces/Symbols.java
@@ -6,7 +6,17 @@ import java.util.Arrays;
 
 public enum Symbols {
     WHITE_PAWN(Colors.WHITE, "♙"),
-    BLACK_PAWN(Colors.BLACK, "♟");
+    BLACK_PAWN(Colors.BLACK, "♟"),
+    WHITE_KNIGHT(Colors.WHITE, "♘"),
+    BLACK_KNIGHT(Colors.BLACK, "♞"),
+    WHITE_ROOK(Colors.WHITE, "♖"),
+    BLACK_ROOK(Colors.BLACK, "♜"),
+    WHITE_BISHOP(Colors.WHITE, "♗"),
+    BLACK_BISHOP(Colors.BLACK, "♝"),
+    WHITE_QUEEN(Colors.WHITE, "♕"),
+    BLACK_QUEEN(Colors.BLACK, "♛"),
+    WHITE_KING(Colors.WHITE, "♔"),
+    BLACK_KING(Colors.BLACK, "♚");
 
     private Colors symbolColor;
     private String symbolShape;
@@ -23,11 +33,11 @@ public enum Symbols {
                 .orElse(null);
     }
 
-    public Colors getPawnColor() {
+    public Colors getPieceColor() {
         return symbolColor;
     }
 
-    public String getSymbolShape() {
+    public String getPieceSymbol() {
         return symbolShape;
     }
 }
