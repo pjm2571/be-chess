46edb550ce43fa7321d34d655beebee1d18751a5
Zoonmy <pjm2571@naver.com>
2024-03-06 13:36:06 +0900

    feat: utils.StringUtils 클래스의 개행문자 리턴 메소드 구현 - appendNewLine() 메소드를 실행하면 개행문자를 리턴해주는 메소드 구현

---
+++ src/main/java/utils/StringUtils.java
@@ -0,0 +1,12 @@
+package utils;
+
+public class StringUtils {
+    private StringUtils() {
+    }
+
+    public static final String NEWLINE = System.lineSeparator();
+
+    public static String appendNewLine() {
+        return NEWLINE;
+    }
+}
