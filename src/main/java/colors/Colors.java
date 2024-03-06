36f187e5a6a2b6ef9a6b76c0494acee810e50e35
Zoonmy <pjm2571@naver.com>
2024-03-06 10:14:27 +0900

    refactor: pawnColors 변수를 pawnColor로 변경

--- src/main/java/colors/Colors.java
+++ src/main/java/colors/Colors.java
@@ -1,17 +1,6 @@
 package colors;
 
 public enum Colors {
-    WHITE("white"),
-    BLACK("black");
-
-    private String colorType;
-
-    Colors(String colorType) {
-        this.colorType = colorType;
-    }
-
-    public String getColorType(){
-        return colorType;
-    }
-
+    WHITE,
+    BLACK;
 }
