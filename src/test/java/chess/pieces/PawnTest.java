143c352364256e7fc2fe12acac631829112f46f1
Zoonmy <pjm2571@naver.com>
2024-03-06 17:16:38 +0900

    refactor: Pawn 객체의 Shape 및 Color를 테스트하는 코드 수정

--- src/test/java/chess/pieces/PawnTest.java
+++ src/test/java/chess/pieces/PawnTest.java
@@ -3,58 +3,38 @@ package chess.pieces;
 import colors.Colors;
 import org.junit.jupiter.api.*;
 
-import java.lang.reflect.Field;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.*;
 
 public class PawnTest {
     @Test
     @DisplayName("흰색 폰과 검정색 폰의 생성 테스트")
-    public void create() throws Exception {
+    public void create(){
         verifyPawn(Colors.WHITE);
         verifyPawn(Colors.BLACK);
     }
 
-    private void verifyPawn(Colors Colors) throws Exception {
-        Pawn pawn = new Pawn(Colors);
+    private void verifyPawn(Colors pawnColor) {
+        Pawn pawn = new Pawn(pawnColor);
 
-        Object pawnColor = getPawnColor(pawn);
-
-        assertEquals(Colors, pawnColor);
-    }
-
-    private Object getPawnColor(Pawn pawn) throws Exception {
-        /* chess/pieces/Pawn.java 클래스를 찾는다 */
-        Class<?> pawnClass = Class.forName("chess.pieces.Pawn");
-
-        /* pawnClass의 pawnColor field를 찾아온다 */
-        Field field = pawnClass.getDeclaredField("pawnColor");
-
-        /* field의 접근 제한자가 private이기 때문에, setAccessible을 true로 설정한다. 하지 않으면 오류! */
-        field.setAccessible(true);
-
-        /* field의 color 값을 가져온다. color enum 타입이므로 Object로 받아온다. */
-        return field.get(pawn);
+        assertThat(pawnColor).isEqualTo(pawn.getPawnColor());
     }
 
     @Test
     @DisplayName("매개변수 없는 폰의 생성 테스트")
-    public void create_with_non_color() throws Exception {
+    public void create_with_non_color() {
         Pawn pawn = new Pawn();
 
-        Object pawnColor = getPawnColor(pawn);
-
-        assertEquals(Colors.WHITE, pawnColor);
+        assertThat(Colors.WHITE).isEqualTo(pawn.getPawnColor());
     }
 
     @Test
-    @DisplayName("흰색 폰의 Shape 테스트")
+    @DisplayName("흰색 폰의 Shape 일치 테스트")
     public void create_pawn_check_shape() {
         Pawn pawn = new Pawn();
 
         String expectedShape = Symbols.WHITE_PAWN.getSymbolShape();
-        
-        assertEquals(expectedShape, pawn.getPawnSymbol());
+
+        assertThat(expectedShape).isEqualTo(pawn.getPawnSymbol());
     }
 
 
