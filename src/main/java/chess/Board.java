cb41455310b65a006ff63d97b357f7808379f6c4
Zoonmy <pjm2571@naver.com>
2024-03-06 22:58:10 +0900

    feat: initialize 수행 시, Piece를 보드에 저장하는 기능, 보드의 초기 화면을 출력하는 기능 구현 - List<List<Piece>>를 이용하여 whitePiece와 blackPiece를 구분하여 저장 - StringBuilder를 사용하여 showBoard 메소드를 구현

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -1,74 +1,84 @@
 package chess;
 
-import chess.pieces.Pawn;
-import chess.colors.Colors;
+import chess.pieces.Piece;
+import chess.pieces.Symbols;
 import utils.StringUtils;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Board {
-    private List<Pawn> pawns = new ArrayList<>();
-    private String[][] chessBoard = new String[8][8];
-
-
-    public void add(Pawn pawn) {
-        pawns.add(pawn);
-    }
-
-    public int size() {
-        return pawns.size();
-    }
-
-    public Pawn findPawn(int pawnIndex) {
-        return pawns.get(pawnIndex);
-    }
+    private final static int BOARD_LENGTH = 8;
+    private List<List<Piece>> pieces = new ArrayList<>();
 
     public void initialize() {
-        setBlank(0);
-        setPawn(1, Colors.BLACK);
-        setBlank(2);
-        setBlank(3);
-        setBlank(4);
-        setBlank(5);
-        setPawn(6, Colors.WHITE);
-        setBlank(7);
+        addBlackPieces();
+        addWhitePieces();
     }
 
-    private void setBlank(int rowIndex) {
-        for (int i = 0; i < 8; i++) {
-            chessBoard[rowIndex][i] = ".";
+    public int getSize() {
+        return pieces.stream()
+                .mapToInt(List::size)
+                .sum();
+    }
+
+    private void addBlackPieces() {
+        List<Piece> blackPieces = new ArrayList<>();
+        blackPieces.add(Piece.createPiece(Symbols.BLACK_ROOK));
+        blackPieces.add(Piece.createPiece(Symbols.BLACK_KNIGHT));
+        blackPieces.add(Piece.createPiece(Symbols.BLACK_BISHOP));
+        blackPieces.add(Piece.createPiece(Symbols.BLACK_QUEEN));
+        blackPieces.add(Piece.createPiece(Symbols.BLACK_KING));
+        blackPieces.add(Piece.createPiece(Symbols.BLACK_BISHOP));
+        blackPieces.add(Piece.createPiece(Symbols.BLACK_KNIGHT));
+        blackPieces.add(Piece.createPiece(Symbols.BLACK_ROOK));
+
+        addPawns(blackPieces, Symbols.BLACK_PAWN);
+
+        pieces.add(blackPieces);
+    }
+
+    private void addWhitePieces() {
+        List<Piece> whitePieces = new ArrayList<>();
+
+        addPawns(whitePieces, Symbols.WHITE_PAWN);
+
+        whitePieces.add(Piece.createPiece(Symbols.WHITE_ROOK));
+        whitePieces.add(Piece.createPiece(Symbols.WHITE_KNIGHT));
+        whitePieces.add(Piece.createPiece(Symbols.WHITE_BISHOP));
+        whitePieces.add(Piece.createPiece(Symbols.WHITE_QUEEN));
+        whitePieces.add(Piece.createPiece(Symbols.WHITE_KING));
+        whitePieces.add(Piece.createPiece(Symbols.WHITE_BISHOP));
+        whitePieces.add(Piece.createPiece(Symbols.WHITE_KNIGHT));
+        whitePieces.add(Piece.createPiece(Symbols.WHITE_ROOK));
+
+        pieces.add(whitePieces);
+    }
+
+    private void addPawns(List<Piece> colorPieces, Symbols symbol) {
+        for (int i = 0; i < BOARD_LENGTH; i++) {
+            colorPieces.add(Piece.createPiece(symbol));
         }
     }
 
-    private void setPawn(int rowIndex, Colors pawnColor) {
-        for (int i = 0; i < 8; i++) {
-            Pawn pawn = new Pawn(pawnColor);
-            add(pawn);
-            chessBoard[rowIndex][i] = pawn.getPawnSymbol();
-        }
-    }
 
-    public String print() {
-        StringBuilder sb = new StringBuilder();
-        Arrays.stream(chessBoard)
-                .forEach(row -> {
-                    Arrays.stream(row).forEach(sb::append);
-                    sb.append(StringUtils.appendNewLine());
-                });
+    public String showBoard() {
+        String pieceShapes = pieces.stream()
+                .flatMap(List::stream)
+                .map(Piece::getPieceShape)
+                .collect(Collectors.joining());
 
-        return sb.toString();
-    }
+        StringBuilder sb = new StringBuilder(pieceShapes);
 
-    public String getPawnSymbols(Colors pawnColor) {
-        StringBuilder sb = new StringBuilder();
-        pawns.stream()
-                .filter(pawn -> pawn.getPawnColor().equals(pawnColor))
-                .map(Pawn::getPawnSymbol) // 각 폰을 문자열로 변환합니다. 예시로 toString() 메서드를 사용했습니다.
-                .forEach(sb::append); // StringBuilder에 추가합니다.
+        sb.insert(BOARD_LENGTH + BOARD_LENGTH, StringUtils.NEWLINE + "........" + StringUtils.NEWLINE + "........" + StringUtils.NEWLINE + "........" + StringUtils.NEWLINE + "........" + StringUtils.NEWLINE);
+
+        sb.insert(BOARD_LENGTH, StringUtils.NEWLINE);
+
+        sb.insert(sb.length() - BOARD_LENGTH, StringUtils.NEWLINE);
 
         return sb.toString();
+
     }
 
 
