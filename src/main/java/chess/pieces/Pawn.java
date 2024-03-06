3adc3ad6376b9ad9c49f6a57cf6423e7213969fe
Zoonmy <pjm2571@naver.com>
2024-03-06 10:31:46 +0900

    feat: Pawn의 Symbol의 테스트 코드를 통과하기 위한 Pawn 메소드 구현 - getPawnSymbol() 메소드를 통해 폰의 Symbol을 판단

--- src/main/java/chess/pieces/Pawn.java
+++ src/main/java/chess/pieces/Pawn.java
@@ -4,12 +4,23 @@ import colors.Colors;
 
 public class Pawn {
     private final Colors pawnColor;
+    private Symbols pawnSymbol;
 
     public Pawn() {
         this.pawnColor = Colors.WHITE;
+        setPawnSymbol();
     }
 
     public Pawn(Colors pawnColors) {
         this.pawnColor = pawnColors;
+        setPawnSymbol();
+    }
+
+    private void setPawnSymbol(){
+        this.pawnSymbol = Symbols.getPawnSymbol(pawnColor);
+    }
+
+    public String getPawnSymbol(){
+        return pawnSymbol.getSymbolShape();
     }
 }
