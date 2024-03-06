a11806bd517457aefdb4c625afa74bd5343c091d
Zoonmy <pjm2571@naver.com>
2024-03-06 23:04:04 +0900

    feat: Board의 piece개수 32개 테스트, showBoard() 출력값 테스트 - board의 piece가 32개가 되는 지 테스트 - board의 showBoard String 값의 일치 여부 테스트

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,14 +1,9 @@
 package chess;
 
+import org.junit.jupiter.api.*;
 import utils.StringUtils;
 
-import chess.pieces.Pawn;
-import chess.colors.Colors;
-
-import org.junit.jupiter.api.*;
-
 import static org.assertj.core.api.Assertions.*;
-
 
 public class BoardTest {
     private Board board;
@@ -19,47 +14,27 @@ public class BoardTest {
     }
 
     @Test
-    @DisplayName("체스 보드 size가 0이 되는 테스트")
-    public void board_size_0() {
-        assertThat(0).isEqualTo(board.size());
-    }
-
-    @Test
-    @DisplayName("체스 보드 size가 100이 되는 테스트")
-    public void board_size_100() {
-        int size = 100;
-        for (int i = 0; i < size; i++) {
-            board.add(new Pawn());
-        }
-        assertThat(size).isEqualTo(board.size());
-    }
-
-    @Test
-    @DisplayName("체스 보드 print() 테스트")
-    public void board_print() {
+    @DisplayName("보드에 위치한 Piece들의 정상 개수 테스트")
+    public void piece_size_test() {
         board.initialize();
 
-        String expectedResult =
-                "........" + StringUtils.NEWLINE +
-                        "♟♟♟♟♟♟♟♟" + StringUtils.NEWLINE +
-                        "........" + StringUtils.NEWLINE +
-                        "........" + StringUtils.NEWLINE +
-                        "........" + StringUtils.NEWLINE +
-                        "........" + StringUtils.NEWLINE +
-                        "♙♙♙♙♙♙♙♙" + StringUtils.NEWLINE +
-                        "........" + StringUtils.NEWLINE;
-
-        assertThat(expectedResult).isEqualTo(board.print());
+        assertThat(board.getSize()).isEqualTo(32);
     }
 
     @Test
-    @DisplayName("initialize 직후, 흰색, 검은색 폰들의 Symbol 테스트")
-    public void find_pawnSymbols() {
+    @DisplayName("Board의 showBoard 정상 출력 테스트")
+    public void showBoard_test() {
         board.initialize();
-        assertThat("♙♙♙♙♙♙♙♙").isEqualTo(board.getPawnSymbols(Colors.WHITE));
-        assertThat("♟♟♟♟♟♟♟♟").isEqualTo(board.getPawnSymbols(Colors.BLACK));
+
+        String blankRank = "........" + StringUtils.NEWLINE;
+        assertThat(board.showBoard()).isEqualTo("♜♞♝♛♚♝♞♜" + StringUtils.NEWLINE +
+                "♟♟♟♟♟♟♟♟" + StringUtils.NEWLINE +
+                blankRank +
+                blankRank +
+                blankRank +
+                blankRank +
+                "♙♙♙♙♙♙♙♙" + StringUtils.NEWLINE +
+                "♖♘♗♕♔♗♘♖");
     }
-
-
 
 }
