618f10f27c6b88d05ea0ec9645316dcbd73fce89
Zoonmy <pjm2571@naver.com>
2024-03-06 11:55:37 +0900

    feat: main 기능 구현 - Scanner를 통한 입력을 받아 동작 	- start : 체스 게임을 시작 	- end : 체스 게임을 종료

---
+++ src/main/java/main.java
@@ -0,0 +1,27 @@
+import chess.Board;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Board board = new Board();
+
+        StringBuilder inputCommand;
+
+        while (true) {
+            System.out.println("시작 : start / 종료 : end   입력 > ");
+            inputCommand = new StringBuilder(sc.nextLine());
+            if (inputCommand.toString().equals("start")) {
+                System.out.println(board.print());
+                // 추가 구현
+
+            } else if (inputCommand.toString().equals("end")) {
+                break;
+            }
+        }
+
+        System.out.println("체스 게임 종료");
+    }
+}
