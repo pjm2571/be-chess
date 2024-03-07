40fbe1c91092e5d0a40579327d549b264bcf2e17
Zoonmy <pjm2571@naver.com>
2024-03-07 14:46:53 +0900

    refactor: Rank 객체를 생성함에 따른 Board 클래스 수정 - List<List<Piece>>를 List<Rank> 로 수정

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 
 public class Board {
     private final static int BOARD_LENGTH = 8;
-    private final List<List<Piece>> pieces = new ArrayList<>();
+    private final List<Rank> pieces = new ArrayList<>();
 
     public void initialize() {
         addBlackPieces();
@@ -19,23 +19,21 @@ public class Board {
     }
 
     public int getSize() {
-        return pieces.stream()
-                .mapToInt(List::size)
-                .sum();
+        return pieces.size();
     }
 
     private void addNoPieces() {
         for (int row = 0; row < 4; row++) {
-            List<Piece> noPieces = new ArrayList<>();
+            ArrayList<Piece> noPieces = new ArrayList<>();
             for (int i = 0; i < 8; i++) {
                 noPieces.add(Piece.createBlank());
             }
-            pieces.add(noPieces);
+            pieces.add(new Rank(noPieces));
         }
     }
 
     private void addBlackPieces() {
-        List<Piece> blackPieces = new ArrayList<>();
+        ArrayList<Piece> blackPieces = new ArrayList<>();
 
         blackPieces.add(Piece.createBlack(Piece.Type.ROOK));
         blackPieces.add(Piece.createBlack(Piece.Type.KNIGHT));
@@ -45,7 +43,7 @@ public class Board {
         blackPieces.add(Piece.createBlack(Piece.Type.BISHOP));
         blackPieces.add(Piece.createBlack(Piece.Type.KNIGHT));
         blackPieces.add(Piece.createBlack(Piece.Type.ROOK));
-        pieces.add(blackPieces);
+        pieces.add(new Rank(blackPieces));
 
         addBlackPawns();
     }
@@ -53,7 +51,7 @@ public class Board {
     private void addWhitePieces() {
         addWhitePawns();
 
-        List<Piece> whitePieces = new ArrayList<>();
+        ArrayList<Piece> whitePieces = new ArrayList<>();
 
         whitePieces.add(Piece.createWhite(Piece.Type.ROOK));
         whitePieces.add(Piece.createWhite(Piece.Type.KNIGHT));
@@ -64,24 +62,24 @@ public class Board {
         whitePieces.add(Piece.createWhite(Piece.Type.KNIGHT));
         whitePieces.add(Piece.createWhite(Piece.Type.ROOK));
 
-        pieces.add(whitePieces);
+        pieces.add(new Rank(whitePieces));
 
     }
 
     private void addBlackPawns() {
-        List<Piece> blackPawns = new ArrayList<>();
+        ArrayList<Piece> blackPawns = new ArrayList<>();
 
         addPawns(blackPawns, Piece.createBlack(Piece.Type.PAWN));
 
-        pieces.add(blackPawns);
+        pieces.add(new Rank(blackPawns));
     }
 
     private void addWhitePawns() {
-        List<Piece> whitePieces = new ArrayList<>();
+        ArrayList<Piece> whitePieces = new ArrayList<>();
 
         addPawns(whitePieces, Piece.createWhite(Piece.Type.PAWN));
 
-        pieces.add(whitePieces);
+        pieces.add(new Rank(whitePieces));
     }
 
     private void addPawns(List<Piece> pawns, Piece pawn) {
@@ -92,12 +90,6 @@ public class Board {
 
 
     public String showBoard() {
-        return pieces.stream()
-                .map(row -> row.stream()
-                        .map(Piece::getRepresentation)
-                        .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
-                        .toString())
-                .map(StringUtils::appendNewLine)
-                .collect(Collectors.joining());
+        return "";
     }
 }
