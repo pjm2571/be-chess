76d5bc0f4329a06e30c4f77c5e026eacb3d2faee
Zoonmy <pjm2571@naver.com>
2024-03-07 14:40:25 +0900

    feat: 체스판의 Row를 구성하는 Rank 구현 - ArrayList<Piece>로 구현 - findPieceCount로 입력한 Piece가 존재하는 개수 반환 - getRankRepresentation을 통해 Rank의 전체 출력 반환

---
+++ src/main/java/chess/Rank.java
@@ -0,0 +1,28 @@
+package chess;
+
+import chess.pieces.Piece;
+import utils.StringUtils;
+
+import java.util.ArrayList;
+
+public class Rank {
+    private ArrayList<Piece> pieces;
+
+    public Rank(ArrayList<Piece> pieces) {
+        this.pieces = pieces;
+    }
+
+    public int findPieceCount(Piece findPiece) {
+        return (int) pieces.stream()
+                .filter(piece -> piece.equals(findPiece))
+                .count();
+    }
+
+    public String getRankRepresentation() {
+        StringBuilder sb = new StringBuilder();
+        pieces.stream().map(Piece::getRepresentation)
+                .forEach(sb::append);
+        return StringUtils.appendNewLine(sb.toString());
+    }
+
+}
