2772a458b9d7554f0a01a6d248170c94f94e7149
Zoonmy <pjm2571@naver.com>
2024-03-07 16:53:09 +0900

    refactor: color 값에 따른 유효한 Piece들만 리턴되도록 수정 - 매개변수로 color을 받아 color값에 해당하는 valid한 Piece만 리턴하도록 구현

--- src/main/java/chess/board/Rank.java
+++ src/main/java/chess/board/Rank.java
@@ -6,6 +6,8 @@ import utils.StringUtils;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
+import static chess.pieces.Piece.*;
+
 public class Rank {
     private ArrayList<Piece> pieces;
 
@@ -34,9 +36,10 @@ public class Rank {
         pieces.set(pieceIndex, piece);
     }
 
-    public ArrayList<Piece> getPieces() {
+    public ArrayList<Piece> getPiecesByColor(Color color) {
         return pieces.stream()
                 .filter(p -> !p.equals(Piece.createBlank())) // 빈 조각이 아닌 것만 필터링
+                .filter(p -> p.getColor().equals(color))
                 .collect(Collectors.toCollection(ArrayList::new)); // 필터링된 결과를 ArrayList로 변환하여 저장
     }
 
