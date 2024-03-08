4245d8192eced62b1300d56f0ea9d19b533e6ff6
Zoonmy <pjm2571@naver.com>
2024-03-08 10:48:44 +0900

    feat: main 기능에서의 move 기능 구현 - start, end, move에 대한 상수 커맨드 작성 - 입력값에 따른 게임 순서 구현 - 체스 게임 라운드 별 종료, 전체 종료 기능 구현

--- src/main/java/chess/ChessGame.java
+++ src/main/java/chess/ChessGame.java
@@ -5,27 +5,48 @@ import chess.board.Board;
 import java.util.Scanner;
 
 public class ChessGame {
+    private final static String MOVE_COMMAND = "move";
+    private final static String START_COMMAND = "start";
+    private final static String END_COMMAND = "end";
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        Board board = new Board();
-
-        StringBuilder inputCommand;
 
         while (true) {
             System.out.println("시작 : start / 종료 : end");
             System.out.print("입력 > ");
-            inputCommand = new StringBuilder(sc.nextLine());
-            if (inputCommand.toString().equals("start")) {
-                board.initialize();
-                System.out.println(board.showBoard());
-                // 추가 구현
 
-            } else if (inputCommand.toString().equals("end")) {
+            if (sc.nextLine().equals(START_COMMAND)) {
+                Board board = new Board();
+
+                board.initialize();
+
+                while (true) {
+                    System.out.println(board.showBoard());
+                    System.out.print("체스판 명령 입력 > ");
+
+                    String[] commands = sc.nextLine().split(" ");
+
+                    if (commands[0].equals(END_COMMAND)) {
+                        System.out.println("체스 게임 종료");
+                        break;
+                    }
+
+                    if (commands[0].startsWith(MOVE_COMMAND)) {
+                        board.movePiece(commands[1], commands[2]);
+                    }
+
+                }
+                // 추가 구현
+            } else if (sc.nextLine().equals(END_COMMAND)) {
                 break;
             }
         }
 
-        System.out.println("체스 게임 종료");
+        System.out.println("체스 게임 전체 종료");
+        sc.close();
+
     }
+
+
 }
